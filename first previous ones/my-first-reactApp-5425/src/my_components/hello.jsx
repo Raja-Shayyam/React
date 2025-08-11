14ce292cdfd1d9ed5320369234fdef295764305b
Rehan Raja <rehman0rehan@gmail.com>
2025-08-11 13:51:26 +0500

    updates vrsion

--- first previous ones/my-first-reactApp-5425/src/my_components/hello.jsx
+++ first previous ones/my-first-reactApp-5425/src/my_components/hello.jsx
@@ -1,3 +1,5 @@
+import { Salam } from "./salam";
+
 export function Hi_hello()
 {
   let name = 'muqeet rehan';
@@ -7,7 +9,7 @@ export function Hi_hello()
 
 //  {'<'}= this should be declare dynamicaly other wise error 
   return <div>
-    <h4>salam Alikum ={'>'} {name} {'<'}= in react.js</h4> 
+    <Salam value={name}/>
     <h4>this name is showing through function {'=> ' + fullName() + ' <='} .</h4>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur cum iure aspernatur sapiente libero </p>
     
---
+++ first previous ones/my-first-reactApp-5425/src/my_components/salam.jsx
@@ -0,0 +1,9 @@
+
+export const Salam = ({value}) => {
+  console.log(value);
+  
+  return (
+    <h4>salam Alikum ={'>'} <span style={{font:"status-bar"}}>{value}</span> {'<'}= in react.js</h4> 
+  )
+}
+
